2ac9d81eab63ced07f62bdbd1064e6372ffb6f3d
Jake Baum <jake@jakebaum.com>
2019-09-25 19:15:08 +0100

    Added GameObject and CollisionBox interfaces

---
+++ Valendra/Valendra/ICollisionBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Valendra
+{
+    interface ICollisionBox
+    {
+        //---------------Attributes---------------
+        Vector2 position { get; set; }
+        Vector2 size { get; set; }
+        IGameObject gameObject { get; set; }
+
+        //---------------Methods---------------
+        List<IGameObject> Collisions();
+        Vector2 WorldCoords();
+    }
+}
---
+++ Valendra/Valendra/IGameObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Valendra
+{
+    interface IGameObject
+    {
+        //---------------Attributes---------------
+        Vector2 position { get; set; }
+        Vector2 speed { get; set; }
+        Vector2 maxSpeed { get; set; }
+        Vector2 size { get; set; }
+        double rotation { get; set; }
+        string textureName { get; set; }
+        IGameObject parent { get; set; }
+        List<IGameObject> children { get; set; }
+        bool isVisible { get; set; }
+        bool isMovable { get; set; }
+        bool isCollidable { get; set; }
+        List<ICollisionBox> collisionBoxes { get; set; }
+
+        //---------------Methods---------------
+        void Update();
+        void Draw();
+        void Move();
+        bool AddToWorld();//Returns true if added to world successfully
+        void Destroy();
+        Vector2 Centre();//Returns coords of centre of object
+        Vector2 WorldPosition();//Returns world coords of object
+
+
+    }
+}
